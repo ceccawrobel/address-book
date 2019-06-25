d711cca615353ee6ece5b57cadbf26ed5a8e9bcc
cecca wrobel and winter runion <cecca+winter@>
2019-06-24 20:59:19 -0700

    add method for generating unique IDs

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 
 //Business Logic For AddressBook ----
 function AddressBook() {
-  this.contacts = []
+  this.contacts = [],
+  this.currentId = 0
 }
 
 AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
   this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
 }
 
 // Business Logic for Contacts ----
